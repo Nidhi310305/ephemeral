bd8ad2d789e5ed00ba655cba975d263babcd48d0
Nidhi Sharma <ns31032005@gmail.com>
2026-07-06 18:24:31 +0530

    Added HeroSection component with styling

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import "./HeroSection.css";
+function HeroSection() {
+    return(
+        <div className="hero">
+         <h1 className="hero-title">Ephemeral</h1>
+         <p className="hero-subtitle">Every masterpiece begins as imagination </p>
+         <p className="hero-tagline">The moment is ephemeral,The creation is forever.</p>
+         <button className="hero-btn">Start Creating </button>
+        </div>
+        
+    )
+}
+
+export default HeroSection;
